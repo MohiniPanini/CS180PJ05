9081a78e4b0527a7b8daaaa86e6d167151b14fa9
McKenna <82552284+kenna1673@users.noreply.github.com>
2021-04-22 18:24:15 -0400

    GUI classes for different user choices

---
+++ editGUI.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * editGUI
+ *
+ * Represents the gui if the user chooses to edit their messages
+ *
+ * @author
+ * @version April 19, 2021
+ */
+
+public class editGUI extends JComponent implements Runnable {
+
+    // Fields
+    private JFrame editFrame;
+
+    // run
+    public void run() {
+
+        // frame, container for when edit is clicked
+        editFrame = new JFrame("Edit messages");
+        Container editContent = editFrame.getContentPane();
+        editContent.setLayout(new BorderLayout());
+        editFrame.setSize(600, 400); // size
+        editFrame.setLocationRelativeTo(null);
+        editFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Label and panel
+        JLabel title = new JLabel("Choose a message to edit");
+        JPanel panel = new JPanel();
+        panel.add(title);
+        editContent.add(panel, BorderLayout.CENTER);
+        editFrame.setVisible(true);
+    } // run
+}
